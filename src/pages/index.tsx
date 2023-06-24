d4f317147e4c55868c1aba0c8d2c1983033b72d0
Leandro Martins <leandro.marttins.85@gmail.com>
2023-06-24 18:15:06 -0300

    Setup & estrutura - Instalação do Design System

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Heading } from '@ignite-ui/react'
 import Head from 'next/head'
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />        
       </Head>
       <main>
-        <h1>Hello World</h1>
+        <Heading as="h1">Hello World</Heading>
       </main>
     </>
   )
